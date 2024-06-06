7b665c2d8a7dbc730b504505566256b7e4017dd7
Michiel Post <michiel@michielpost.nl>
2024-06-06 18:47:00 +0200

    create token

--- Pages/Home.razor.cs
+++ Pages/Home.razor.cs
@@ -1,4 +1,6 @@
 ﻿using ArweaveBlazor;
+using ArweaveBlazor.Models;
+using System.Net;
 
 namespace FlowbiteBlazorWasmStarter.Pages
 {
@@ -9,10 +11,13 @@ namespace FlowbiteBlazorWasmStarter.Pages
         string? _address = null;
         string? _resultId = null;
         string? _resultMsg = null;
+        string? _newTokenId = null;
         string? _tokenResult = null;
 
         string morpheus = "-xmOkXs3b6vMnQmdMw4tasHVZwG3_d7kEiUJa9lp8_w";
         private string inputName { get; set; } = string.Empty;
+        private string tokenName { get; set; } = string.Empty;
+        private string tokenTicker { get; set; } = string.Empty;
         public void Step(int step)
         {
             _step = step;
@@ -72,8 +77,39 @@ namespace FlowbiteBlazorWasmStarter.Pages
                 return;
             }
 
-            //TODO: Send to process
-            _tokenResult = "tokenId";
+            //string data = "local bint = require('.bint')(256)\r\nlocal ao = require('ao')\r\nlocal json = require('json')\r\n\r\nHandlers.add('talk', Handlers.utils.hasMatchingTag('Action', 'talk'),\r\n  function(msg) \r\n    \r\n    ao.send({ Target = msg.From, Data = \"Hi \" .. msg.Tags.Name .. \", are you The One? Can you create a token for me?\"}) \r\nend)";
+            string data = EmbeddedResourceReader.ReadResource("FlowbiteBlazorWasmStarter.token.txt");
+            data = data.Replace("ao.id", $"\"{_address}\"");
+            data = data.Replace("_NAME_", tokenName);
+            data = data.Replace("_TICKER_", tokenTicker);
+
+            Console.WriteLine(tokenName);
+            Console.WriteLine(tokenTicker);
+            Console.WriteLine(data);
+
+            _newTokenId = await ArweaveService.CreateProcess(_jwk, "nI_jcZgPd0rcsnjaHtaaJPpMCW847ou-3RGA5_W3aZg");
+            Console.WriteLine("processId: " + _newTokenId);
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            var dataId = await ArweaveService.SendAsync(_jwk, _newTokenId, _address, data, new List<Tag>
+            {
+                new Tag() { Name = "Action", Value = "Eval"}
+            });
+            Console.WriteLine("DataId: " + dataId);
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            var testResult = await ArweaveService.SendAsync(_jwk, _newTokenId, null, null, new List<ArweaveBlazor.Models.Tag>
+            {
+                new ArweaveBlazor.Models.Tag { Name = "Target", Value = _newTokenId},
+                new ArweaveBlazor.Models.Tag { Name = "Action", Value = "Transfer"},
+                new ArweaveBlazor.Models.Tag { Name = "Quantity", Value = "10000"},
+                new ArweaveBlazor.Models.Tag { Name = "Recipient", Value = _address},
+            });
+            Console.WriteLine("testResult: " + testResult);
+
+            _tokenResult = _newTokenId;
 
         }
 
---
+++ pocesses/readme.txt
@@ -0,0 +1,12 @@
+ -xmOkXs3b6vMnQmdMw4tasHVZwG3_d7kEiUJa9lp8_w
+aos morph2 
+.load morpheus.lua
+
+Send({ Target = "dYfHdjT3eiJRgZeKKke5vvBgedyJVgSCC5TgTmXUMU0", Tags = { Action = "Talk" }, Data = "ping" })
+
+Send({ Target = "dYfHdjT3eiJRgZeKKke5vvBgedyJVgSCC5TgTmXUMU0", Data = "ping" })
+
+
+ Send({ Target = "-xmOkXs3b6vMnQmdMw4tasHVZwG3_d7kEiUJa9lp8_w", Tags = { Action = "Talk", Name = "test" }, Data = "ping" })
+
+Send({ Target = "hICycfjEXgUKrCOtiOrwDtr_iIoNQE7TO-wJPqcQV7U", Tags = { Action = "Talk", Name = "test" }, Data = "ping" })
