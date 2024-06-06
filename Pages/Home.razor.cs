8f54e8cb2f9f78dbf3e758a4ef7b54925681985c
Michiel Post <michiel@michielpost.nl>
2024-06-06 10:43:53 +0200

    updated blazor

--- Pages/Home.razor.cs
+++ Pages/Home.razor.cs
@@ -2,9 +2,25 @@
 {
     public partial class Home
     {
-        public void GenerateWallet()
-        {
+        int _step = 0;
+        string? _jwk = null;
+        string? _address = null;
 
+        public void Step(int step)
+        {
+            _step = step;
+        }
+
+        public async Task GenerateWallet()
+        {
+            _jwk = await ArweaveService.GenerateWallet();
+            _address = await ArweaveService.GetAddress(_jwk);
+        }
+
+        public async Task DownloadWallet()
+        {
+            _jwk = await ArweaveService.GenerateWallet();
+            _address = await ArweaveService.GetAddress(_jwk);
         }
     }
 }
